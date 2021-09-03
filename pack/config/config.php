cfd89d72308819509874b50f4e1546cc2f170b83
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-09-04 00:09:47 +0430

    Add config and working on database

---
+++ pack/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database config
+$server = "localhost";
+$user = "amir";
+$passwd = "amir";
+$db = "narontodo";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
