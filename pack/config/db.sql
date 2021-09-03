4b3032186ef28f6d486a9f56c45b8ac704f85a68
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-09-04 00:32:55 +0430

    Add user table

--- pack/config/db.sql
+++ pack/config/db.sql
@@ -1,3 +1,13 @@
-CREATE DATABASE people (
-    row
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+CREATE TABLE `people`
+(
+    `row`       int(11) NOT NULL AUTO_INCREMENT,
+    `name`      text DEFAULT NULL,
+    `email`     text DEFAULT NULL,
+    `password`  text DEFAULT NULL,
+    PRIMARY KEY (`row`)
 );
