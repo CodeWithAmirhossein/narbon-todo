565630ad3cbf0e90183fa3fdcb250daa1c87014b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-09-04 17:19:49 +0430

    Change database name

--- pack/config/config.php
+++ pack/config/config.php
@@ -4,7 +4,7 @@
 $server = "localhost";
 $user = "amir";
 $passwd = "amir";
-$db = "narontodo";
+$db = "narbontodo";
 
 // Create connection
 $connection = mysqli_connect($server, $user, $passwd, $db);
