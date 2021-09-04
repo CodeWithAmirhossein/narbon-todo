778d37980498368d89656e12c97387a2c622c06e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-09-04 17:11:30 +0430

    Add id in table

--- pack/config/db.sql
+++ pack/config/db.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS `people`;
 CREATE TABLE `people`
 (
     `row`       int(11) NOT NULL AUTO_INCREMENT,
+    `id`        text DEFAULT NULL,
     `name`      text DEFAULT NULL,
     `email`     text DEFAULT NULL,
     `password`  text DEFAULT NULL,
