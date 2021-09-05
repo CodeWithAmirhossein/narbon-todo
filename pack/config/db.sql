1382cc9ead9f30099d25138b32f92dbbe75b3f41
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-09-05 17:38:51 +0430

    Add tasks table

--- pack/config/db.sql
+++ pack/config/db.sql
@@ -22,3 +22,18 @@ CREATE TABLE `people`
 --
 
 INSERT INTO `people` (`id`, `name`, `email`, `password`) VALUES ('123456', 'امیرحسین محمدی', 'amirhosseinmohammadi1380@yahoo.com', 'amir2003');
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE `tasks`
+(
+    `row`           int(11) NOT NULL AUTO_INCREMENT,
+    `taskid`        text DEFAULT NULL,
+    `userid`        text DEFAULT NULL,
+    `name`          text DEFAULT NULL,
+    `status`         text DEFAULT NULL,
+    PRIMARY KEY (`row`)
+);
