8ce2c0486064c04d1873ae3a59dde0878ef08803
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-26 22:35:37 +0330

    Add changeTab for items

--- index.php
+++ index.php
@@ -57,13 +57,13 @@ if (isset($_GET['trash'])) {
 
 <body class="main">
         <p>
-                <span class="text-primary"><i class="fa fa-plus"></i> Add</span>
+                <span onclick="return changeTab('add');" class="text-primary"><i class="fa fa-plus"></i> Add</span>
                 &nbsp;
-                <span class="text-dark"><i class="fa fa-home"></i> Home</span>
+                <span onclick="return changeTab('home');" class="text-dark"><i class="fa fa-home"></i> Home</span>
                 &nbsp;
-                <span class="text-success"><i class="fa fa-check"></i> Done</span>
+                <span onclick="return changeTab('done');" class="text-success"><i class="fa fa-check"></i> Done</span>
                 &nbsp;
-                <span class="text-danger right"><i class="fa fa-trash"></i> Trash</span>
+                <span onclick="return changeTab('trash');" class="text-danger right"><i class="fa fa-trash"></i> Trash</span>
         </p>
         <hr>
 
