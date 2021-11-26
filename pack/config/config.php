0429f445575bd28cef6966ac402eca8715349161
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-26 20:48:56 +0330

    Create connection

---
+++ pack/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('server', 'localhost');
+define('user', 'amir');
+define('password', 'amir');
+define('database', 'donit');
+
+$connection = mysqli_connect(server, user, password, database);
