d99d1cb05c05ee506b8318ec9a85b3bc9a6191e0
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-26 22:50:22 +0330

    Remove pack

--- pack/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('server', 'localhost');
-define('user', 'amir');
-define('password', 'amir');
-define('database', 'donit');
-
-$connection = mysqli_connect(server, user, password, database);
